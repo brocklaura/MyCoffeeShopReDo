da0a919182236ec961683b361f032faa11f477ba
Laura Brock <laurabrock@quickenloans.com>
2020-04-06 13:45:28 -0400

    second commit

---
+++ MyCoffeeShop/Models/Feedback.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCoffeeShop.Models
+{
+    public class Feedback
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Rating { get; set; }
+        public string FeedbackMsg { get; set; }
+
+    }
+}
